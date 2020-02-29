0ffa1309fb20e7c11c0f28b9debaa2a088b42082
Carter <Carterblair4@gmail.com>
2020-02-29 14:43:00 -0800

    cleaned up

--- app/main.py
+++ app/main.py
@@ -8,41 +8,22 @@ from api import ping_response, start_response, move_response, end_response
 
 @bottle.route('/')
 def index():
-    return '''
-    Battlesnake documentation can be found at
-       <a href="https://docs.battlesnake.com">https://docs.battlesnake.com</a>.
-    '''
+    return
 
 
 @bottle.route('/static/<path:path>')
 def static(path):
-    """
-    Given a path, return the static file located relative
-    to the static folder.
-
-    This can be used to return the snake head URL in an API response.
-    """
     return bottle.static_file(path, root='static/')
 
 
 @bottle.post('/ping')
 def ping():
-    """
-    A keep-alive endpoint used to prevent cloud application platforms,
-    such as Heroku, from sleeping the application instance.
-    """
     return ping_response()
 
 
 @bottle.post('/start')
 def start():
     data = bottle.request.json
-
-    """
-    TODO: If you intend to have a stateful snake AI,
-            initialize your snake state here using the
-            request's data if necessary.
-    """
     # print(json.dumps(data))
 
     return {
@@ -55,11 +36,6 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-
-    """
-    TODO: Using the data from the endpoint request object, your
-            snake AI must choose a direction to move in.
-    """
     # print(json.dumps(data))
 
     directions = ['up', 'down', 'left', 'right']
@@ -72,12 +48,7 @@ def move():
 @bottle.post('/end')
 def end():
     data = bottle.request.json
-
-    """
-    TODO: If your snake AI was stateful,
-        clean up any stateful objects here.
-    """
-    print(json.dumps(data))
+    # print(json.dumps(data))
 
     return end_response()
 
